d0b08998606563e5781f6e035906dea2eff4dfed
XeroAcc <sumit.dey@ryerson.ca>
2018-01-11 09:22:22 -0500

    Add other chart component data p2

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [160, -50, 640, -200, 260, 150], label: 'ECG [μV]' },
+];
+
+const LINE_CHART_EPOCH_LABELS: string[] = ['P', 'Q', 'R', 'S', 'T', 'U'];
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +15,16 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_EPOCH_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+  };
+
+  lineChartLegend: true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;
 
   ngOnInit() {
   }
---
+++ src/app/shared/chart.colors.ts
@@ -0,0 +1,10 @@
+export const LINE_CHART_COLORS = [
+  {
+    backgroundColor: 'rgba(6, 214, 160, 0.2)',
+    borderColor: 'rgba(0, 200, 140, 0.5)',
+    pointBackgroundColor: '#000',
+    pointBorderColor: '#000',
+    pointHoverBackgroundColor: '#555',
+    pointHoverBorderColor: '#555'
+  },
+];
